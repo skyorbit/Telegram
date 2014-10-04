046c6b3c5b7ba8f5d73a2357b46563361efe268f
DevConcert <devconcert@devconcert.net>
2014-10-04 18:23:37 +0900

    Fix. display wrong date..

---
+++ TMessagesProj/src/main/java/org/telegramkr/passcodelock/core/PageListener.java
@@ -0,0 +1,20 @@
+package org.telegramkr.passcodelock.core;
+
+import android.app.Activity;
+
+public interface PageListener {
+
+	void onActivityCreated(Activity activity);
+
+	void onActivityStarted(Activity activity);
+
+	void onActivityResumed(Activity activity);
+
+	void onActivityPaused(Activity activity);
+
+	void onActivityStopped(Activity activity);
+
+	void onActivitySaveInstanceState(Activity activity);
+
+	void onActivityDestroyed(Activity activity);
+}
